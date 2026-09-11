39943153e2a5746673e3a9d4ec3357ae15b3fe4e
Ansel Chen <73325489+chenans1@users.noreply.github.com>
2026-09-11 18:27:43 -0400

    feat: implemented param based radius check
    also popping the proj off the map when found. Also done in on kill as a fall back for arrows that somehow do despawn or otherwise.

--- src/arrowHook.h
+++ src/arrowHook.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "utils.h"
 #include "hitDataAPI.h"
+#include "processEventHook.h"
 
 class arrowHook{
     public:
@@ -14,17 +15,32 @@ class arrowHook{
     private:
         static void AddImpact(RE::ArrowProjectile* a_projectile, RE::TESObjectREFR* a_ref, const RE::NiPoint3& a_targetLoc, const RE::NiPoint3& a_velocity, RE::hkpCollidable* a_collidable, std::int32_t a_arg6, std::uint32_t a_arg7) {
             _original(a_projectile, a_ref, a_targetLoc, a_velocity, a_collidable, a_arg6, a_arg7);
+            
+            //check if we are one of the arrows in arrow rain, if not ignore. 
+            float search_radius;
+            {
+                std::scoped_lock lock(arrow::ProcessEventHook::ARmutex);
+
+                auto it = arrow::ProcessEventHook::ARarrows.find(a_projectile);
+                if (it == arrow::ProcessEventHook::ARarrows.end()) {
+                    return;
+                }
+
+                search_radius = it->second;
+                arrow::ProcessEventHook::ARarrows.erase(it);
+            }
             const RE::NiPoint3 impactPoint = a_targetLoc;
             // SKSE::log::info("[ArrowImpactHook] impact ref={:08X}, position=({}, {}, {})",
             //     a_ref ? a_ref->GetFormID() : 0, impactPoint.x, impactPoint.y, impactPoint.z);
-            const std::vector<RE::ActorHandle> actorsVec = utils::FindActorsNearImpact(a_projectile, impactPoint, 512.0f);
+            const std::vector<RE::ActorHandle> actorsVec = utils::FindActorsNearImpact(a_projectile, impactPoint, search_radius);
             const auto* directlyHitActor = a_ref ? a_ref->As<RE::Actor>() : nullptr;
             const auto shooter = a_projectile ->GetProjectileRuntimeData().shooter.get();
             if (!shooter) {
                 SKSE::log::info("[ArrowImpactHook] AddImpact: no shooter");
                 return;
             }
-            for (const auto& actorHandle : actorsVec) {     
+
+            for (const auto& actorHandle : actorsVec) {
                 auto actor = actorHandle.get();
                 if (!actor) {
                     SKSE::log::warn("[ArrowImpactHook] AddImpact: invalid actor");
